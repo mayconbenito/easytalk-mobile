5b9b7fbea3b79b7f16cd09c023116ad22d057d6e
Maycon Benito <mayki17321@gmail.com>
2020-01-30 11:27:20 -0300

    chore: remove socket.io warning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import Routes from './routes';
 import NavigationService from './services/navigation';
 import { store, persistor } from './store';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
 
 export default function() {
   return (
