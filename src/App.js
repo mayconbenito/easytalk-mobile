ba92eecd17c7a1a7bb03c9feba62c054b8681316
Maycon Benito <mayki17321@gmail.com>
2020-02-10 14:03:26 -0300

    feat: app apk updater

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { YellowBox } from 'react-native';
+import React, { useEffect } from 'react';
+import { YellowBox, Alert } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as UpdateAPK from 'rn-update-apk';
 
 import Routes from './routes';
 import NavigationService from './services/navigation';
@@ -10,6 +11,33 @@ import { store, persistor } from './store';
 YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
 
 export default function() {
+  const updaterURL = 'http://android-app-update.easytalk.ml';
+
+  useEffect(() => {
+    const updater = new UpdateAPK.UpdateAPK({
+      apkVersionUrl: updaterURL,
+      fileProviderAuthority: 'com.mayconbenito.easytalk.provider',
+      needUpdateApp: needUpdate => {
+        Alert.alert('Nova Atualização disponivel', '', [
+          { text: 'Cancelar', onPress: () => {} },
+          {
+            text: 'Atualizar',
+            onPress: () => needUpdate(true),
+          },
+        ]);
+      },
+      forceUpdateApp: async () => {
+        Alert.alert('Download de atualização obrigátoria iniciado');
+      },
+      onError: err => {
+        console.log(err);
+        Alert.alert('Erro ao instalar a atualização');
+      },
+    });
+
+    updater.checkUpdate();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
