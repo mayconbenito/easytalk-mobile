11bdf0ba46a8913c6d16c42b03eb172d69251ba2
Maycon Benito <mayki17321@gmail.com>
2020-02-11 12:20:10 -0300

    release 1.0.0

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { API_URL } from 'react-native-dotenv';
 
 import { store } from '~/store';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.30:3000',
+  baseURL: API_URL || 'http://192.168.15.30:3000',
 });
 
 api.interceptors.request.use(function(config) {
